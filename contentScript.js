8cd5082b1734b9a873a255511849f462ae1d4493
CheshireWho <brodbeck.birgit@gmail.com>
2024-08-18 12:06:12 +0200

    Add simple writing practice to daily vocab trainer

--- contentScript.js
+++ contentScript.js
@@ -46,9 +46,67 @@ const initGoogleTranslateLinkCreation = () => {
     return addGoogleTranslateLink;
 };
 
+const initVocabTrainerAddWriting = () => {
+    const inputClass = 'writing-exercise';
+    let lastTypedText = '';
+
+    const getInputElem = () => {
+        const inputElem = document.createElement('input');
+        inputElem.type = 'text';
+        inputElem.autocapitalize = 'off';
+        inputElem.classList.add(inputClass);
+
+        return inputElem;
+    };
+
+    const blurHandler = (event) => {
+        lastTypedText = event.target.value || '';
+    };
+
+    const addVocabTrainerWritingInput = ({ observer, observerTargetNode, observerConfig }) => {
+        // stop MutationObserver
+        // Keeping it running while manipulating the DOM would cause an infinite loop.
+        observer.disconnect();
+
+        // selects the vocabulary text container in the "blitz" trainer in the daily trainer
+        const selectorDayTrainerSingleVocab = `#containerblitz .DivAutopilotText:not(:has(.${inputClass}))`;
+        const elemsToExtend = document.querySelectorAll(`${selectorDayTrainerSingleVocab}`);
+
+        elemsToExtend.forEach((elem) => {
+            const germanTextElem = elem.querySelector('.DivAutopilotWort1');
+            const l2TextElem = elem.querySelector('.DivAutopilotWort2');
+            if (!germanTextElem) return;
+
+            const inputElem = getInputElem();
+            germanTextElem.appendChild(inputElem);
+
+            const l2Text = l2TextElem.textContent?.trim();
+
+            if (l2Text?.length > 0) {
+                // L2 text is visible. I.e. the DOM has been recreated and the input field with the user's typed text is replaced with a new input field.
+                // Add last typed text to the newly created input field, so that it can be checked against the L2 solution.
+                inputElem.value = lastTypedText;
+
+                if (lastTypedText === l2Text) {
+                    inputElem.classList.add('correct');
+                }
+            } else {
+                // L2 is still hidden. Listen to blur when user stopped typing to store the typed text.
+                inputElem.addEventListener('blur', blurHandler);
+            }
+        });
+
+        // restart MutationObserver
+        observer.observe(observerTargetNode, observerConfig);
+    };
+
+    return addVocabTrainerWritingInput;
+};
+
 (function() {
-    // `addGoogleTranslateLink` is called often, but the other init code is called only once for performance
+    // the returned functions are called often, but the other init code is called only once for performance
     const addGoogleTranslateLink = initGoogleTranslateLinkCreation();
+    const addVocabTrainerWritingInput = initVocabTrainerAddWriting();
 
     const init = () => {
 		const observerTargetNode = document.body;
@@ -57,8 +115,8 @@ const initGoogleTranslateLinkCreation = () => {
         // before making any DOM changes and then re-connect.
 		const observerConfig = { attributes: false, childList: true, subtree: true };
 		const mutationsCallback = (mutationsList, observer) => {
-            console.log('MutationObserver callback triggered');
 			addGoogleTranslateLink({ observer, observerTargetNode, observerConfig });
+			addVocabTrainerWritingInput({ observer, observerTargetNode, observerConfig });
 		};
 		const observer = new MutationObserver(mutationsCallback);
 		observer.observe(observerTargetNode, observerConfig);
