fffa7d4a4a7fa10d7410fa93ec99ef8df4a3fb28
CheshireWho <CheshireWho@users.noreply.github.com>
2025-04-07 13:58:44 +0200

    Add cta to vocab writing field to trigger blue button directly

--- content-scripts/features/vocab-trainer-writing-exercise.js
+++ content-scripts/features/vocab-trainer-writing-exercise.js
@@ -2,18 +2,34 @@
  * Add a writing exercise to the daily "blitz" vocab trainer
  */
 const initVocabTrainerAddWriting = () => {
-    const inputClass = 'writing-exercise';
+    const exerciseElemClass = 'writing-exercise';
     let typedText = '';
 
-    const getInputElem = () => {
+    const getExerciseElem = () => {
         const inputElem = document.createElement('input');
         inputElem.type = 'text';
         inputElem.autocapitalize = 'off';
         inputElem.autocomplete = 'off';
         inputElem.classList.add('eingabezeile1');
-        inputElem.classList.add(inputClass);
 
-        return inputElem;
+        const submitElem = document.createElement('a');
+        submitElem.textContent = 'Test';
+        submitElem.classList.add('ctabutton', 'ctaklein2', 'eingabezeile2');
+
+        // Clicking a button on the page programmatically doesn't work because that button's href calls a function in the 'main'
+        // world which the 'isolated' extension doesn't have access to. But adding that same href function to an element works -_-
+        // This href value is the same as the blue lightbulb button's.
+        const translationButton = document.querySelector('a:has(> i.fa-stack > svg.svg-inline--fa.fa-lightbulb-on)');
+        if (translationButton) {
+            submitElem.href = translationButton.href;
+        }
+
+        const exerciseElem = document.createElement('div');
+        exerciseElem.classList.add(exerciseElemClass);
+        exerciseElem.append(inputElem);
+        exerciseElem.append(submitElem);
+
+        return exerciseElem;
     };
 
     const blurHandler = (event) => {
@@ -26,7 +42,7 @@ const initVocabTrainerAddWriting = () => {
         observer.disconnect();
 
         // selects the vocabulary text container in the "blitz" trainer in the daily trainer
-        const selectorDayTrainerSingleVocab = `#containerblitz .DivAutopilotText:not(:has(.${inputClass}))`;
+        const selectorDayTrainerSingleVocab = `#containerblitz .DivAutopilotText:not(:has(.${exerciseElemClass}))`;
         const elemsToExtend = document.querySelectorAll(`${selectorDayTrainerSingleVocab}`);
 
         elemsToExtend.forEach((elem) => {
@@ -34,15 +50,26 @@ const initVocabTrainerAddWriting = () => {
             const l2TextElem = elem.querySelector('.DivAutopilotWort2');
             if (!germanTextElem) return;
 
-            const inputElem = getInputElem();
-            germanTextElem.appendChild(inputElem);
+            const exerciseElem = getExerciseElem();
+            germanTextElem.append(exerciseElem);
+
+            const inputElem = exerciseElem.querySelector('input');
+            if (!inputElem) return;
 
             const l2Text = l2TextElem.textContent?.trim();
 
             if (l2Text?.length > 0) {
-                // L2 text is visible. I.e. the DOM has been recreated and the input field with the user's typed text is replaced with a new input field.
+                // L2 text is visible. I.e. the DOM has been recreated and the input field with the user's typed text is replaced
+                // with a new input field.
+
                 // Add last typed text to the newly created input field, so that it can be checked against the L2 solution.
                 inputElem.value = typedText;
+
+                // hide the CTA because there's no need to click it again
+                const cta = exerciseElem.querySelector('a');
+                if (cta) {
+                    cta.classList.add('hide');
+                }
 
                 if (typedText === l2Text) {
                     inputElem.classList.add('correct');
