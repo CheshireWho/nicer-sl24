74f36afa1905f8b73c0f4b3a2fb9826f5a41746a
CheshireWho <CheshireWho@users.noreply.github.com>
2025-04-07 13:58:43 +0200

    Add features also to dynamically loaded content

--- contentScript.js
+++ contentScript.js
@@ -1,35 +1,68 @@
-(async function() {
-    const addGoogleTranslateLink = () => {
-        const linkClass = 'gt-link';
-        const sourceLanguage = 'da'; // TODO make dynamic
-        const targetLanguage = 'de';
+const initGoogleTranslateLinkCreation = () => {
+    const gTLinkClass = 'gt-link';
+    const gTSourceLanguage = 'da'; // TODO make dynamic
+    const gTTargetLanguage = 'de';
 
+    const getGTLinkElem = () => {
+        const Icon = document.createElement('i');
+        Icon.classList.add('fa-solid', 'fa-language');
+        const GTLinkElem = document.createElement('a');
+        GTLinkElem.classList.add(gTLinkClass);
+        GTLinkElem.target = '_blank';
+        GTLinkElem.appendChild(Icon);
 
-        const clickHandler = (event) => {
-            const Link = event.target?.closest(`.${linkClass}`);
-            const textToTranslate = Link?.parentElement.textContent?.trim();
-            
-            if (Link && textToTranslate) {
-                Link.href = `https://translate.google.com/?sl=${sourceLanguage}&tl=${targetLanguage}&op=translate&text=${encodeURI(textToTranslate)}`;
-            }
-        };
-
-        const elemsToGetLink = document.querySelectorAll('.verbzelletd:nth-child(2)');
-
-        elemsToGetLink.forEach((elem) => {
-            const Icon = document.createElement('i');
-            Icon.classList.add('fa-solid', 'fa-language');
-            const GTLinkElem = document.createElement('a');
-            GTLinkElem.classList.add(linkClass);
-            GTLinkElem.target = '_blank';
-            GTLinkElem.appendChild(Icon);
-
-            elem.appendChild(GTLinkElem);
-
-            GTLinkElem.addEventListener('click', clickHandler);
-        });
-
+        return GTLinkElem;
     };
 
-    addGoogleTranslateLink();
+    const gTClickHandler = (event) => {
+        const Link = event.target?.closest(`.${gTLinkClass}`);
+        const textToTranslate = Link?.parentElement.textContent?.trim();
+        
+        if (Link && textToTranslate) {
+            Link.href = `https://translate.google.com/?sl=${gTSourceLanguage}&tl=${gTTargetLanguage}&op=translate&text=${encodeURI(textToTranslate)}`;
+        }
+    };
+
+    const addGoogleTranslateLink = ({ observer, observerTargetNode, observerConfig }) => {
+        // stop MutationObserver
+        // Keeping it running while manipulating the DOM would cause an infinite loop.
+        observer.disconnect();
+
+        // selects the L2 cells of the verb trainer that don't have a Google Translate link
+        const selectorVerbTrainer = `.verbzelletd:nth-child(2):not(:has(.${gTLinkClass}))`;
+        const elemsToGetLink = document.querySelectorAll(`${selectorVerbTrainer}`);
+
+        elemsToGetLink.forEach((elem) => {
+            const GTLinkElem = getGTLinkElem();
+            elem.appendChild(GTLinkElem);
+
+            GTLinkElem.addEventListener('click', gTClickHandler);
+        });
+
+        // restart MutationObserver
+        observer.observe(observerTargetNode, observerConfig);
+    };
+
+    return addGoogleTranslateLink;
+};
+
+(function() {
+    // `addGoogleTranslateLink` is called often, but the other init code is called only once for performance
+    const addGoogleTranslateLink = initGoogleTranslateLinkCreation();
+
+    const init = () => {
+		const observerTargetNode = document.body;
+		// Making changes to the DOM triggers a mutation. I.e. it's easy to create an infinite loop!
+        // To avoid that, either set the corresponding option here to `false`, or disconnent the observer
+        // before making any DOM changes and then re-connect.
+		const observerConfig = { attributes: false, childList: true, subtree: true };
+		const mutationsCallback = (mutationsList, observer) => {
+            console.log('MutationObserver callback triggered');
+			addGoogleTranslateLink({ observer, observerTargetNode, observerConfig });
+		};
+		const observer = new MutationObserver(mutationsCallback);
+		observer.observe(observerTargetNode, observerConfig);
+	};
+
+    init();
 })();
